9d55275bca5d0f4ef7cd1824765fef5cd720f711
Andres Vasquez <aaevsqz@gmail.com>
2017-11-09 16:54:03 -0400

    Cambio middleware

--- server.js
+++ server.js
@@ -8,15 +8,12 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   var now = new Date().toString();
-  var log = `${now} ${req.method} ${req.url}`;
+  var log = `${now}: ${req.method} ${req.url}`;
+
   console.log(log);
-  fs.appendFile('server.log', log + '\n',(err) => {
-    if (err) {
-      console.log("Problema para establecer conexion");
-    }
-  } );
+  fs.appendFile('server.log', log + '\n');
   next();
 });
 
